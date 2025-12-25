512d3c75ff48493af275f338a42034a1d4c49bcc
4SightDigital <da@4sightdigital.in>
2025-12-25 16:25:00 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline flex-shrink">
+      i am react working fine
+    </h1>
+     </>
+  )
+}
+
+export default App
